7a5c62287259911d79a457b86f210e2a410d7828
manassorn <manassorn.vanichdilokkul@thomsonreuters.com>
2016-11-25 17:51:17 +0700

    replace app-root-path with cta-common.root

--- test/lib/bricks/businesslogics/schedule/synchronizer.broadcast.test.js
+++ test/lib/bricks/businesslogics/schedule/synchronizer.broadcast.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const appRootPath = require('app-root-path').path;
+const appRootPath = require('cta-common').root('cta-app-schedulerdataservice');
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const nodepath = require('path');
